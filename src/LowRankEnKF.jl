3062b41c5a9d7330647b8aae84a82d53707e56ff
Hanieh Mousavi <hanieh.mousavi93@gmail.com>
2024-01-02 18:43:52 -0800

    LREnKF

--- src/LowRankEnKF.jl
+++ src/LowRankEnKF.jl
@@ -1,5 +1,34 @@
 module LowRankEnKF
 
-# Write your package code here.
+using Interpolations
+using JLD
+using LinearAlgebra
+using ProgressMeter
+using Statistics
+using NamedColors
+using Distributions
+using Combinatorics
+using UnPack
 
-end
+abstract type AbstractConfig end
+abstract type SingularityConfig <: AbstractConfig end
+
+
+
+include("ensemble.jl")
+include("forecast.jl")
+include("observation.jl")
+
+include("DA/types.jl")
+include("DA/generate_twin_experiment.jl")
+include("DA/enkf.jl")
+include("DA/state_utilities.jl")
+include("DA/classification.jl")
+include("DA/MCMC.jl")
+
+
+
+
+
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
-using LowRankEnKF
 using Test
 
-@testset "LowRankEnKF.jl" begin
-    # Write your tests here.
+using LinearAlgebra, Statistics
+using LowRankEnKF
+#using TransportBasedInference
+using ForwardDiff
+
+const GROUP = get(ENV, "GROUP", "All")
+
+if GROUP == "All" || GROUP == "Ensemble"
+  include("ensemble.jl")
 end
